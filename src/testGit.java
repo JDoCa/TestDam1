6b74c7791725914af6c37acb2c7ba9d80b1cd408
JDoCa <jorgedomenech85@gmail.com>
2023-05-30 09:38:41 +0200

    Package: Help Class UserManage.java added & UserManageView.java added.

--- src/testGit.java
+++ src/testGit.java
@@ -1,5 +1,6 @@
 public class testGit {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Dulce Manía - No Vale la Pena");
     }
 }
